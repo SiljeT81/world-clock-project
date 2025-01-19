1dc14a41eb1b9890890ea93d59ea249152e78494
SiljeT81 <siljebea@gmail.com>
2025-01-20 00:10:27 +0100

    Added time and date in JS, minor CSS changes

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+function updateTime() {
+  //London
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".time");
+  let londonTime = moment().tz("Europe/London");
+
+  londonDateElement.innerHTML = londonTime.format("Do MMMM YYYY");
+  londonTimeElement.innerHTML = londonTime.format("HH:mm:ss");
+
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("Do MMMM YYYY");
+  parisTimeElement.innerHTML = parisTime.format("HH:mm:ss");
+}
+
+updateTime();
+setInterval(updateTime, 1000);
